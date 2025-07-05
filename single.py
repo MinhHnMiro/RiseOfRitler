f3bcb50cf6a4fd3a4a6d456cc2172a070a48c998
MinhHnMiro <119595286+MinhHnMiro@users.noreply.github.com>
2025-07-05 19:36:39 +0300

    Add files via upload

--- single.py
+++ single.py
@@ -1,6 +1,7 @@
 import pygame, time, keyboard, socket, random
 from math import sqrt
 
+state = 'menu'
 with open('Assets/MEM/Settings.txt') as g:
     f = g.read().split('\n')
     pygame.init()
@@ -129,127 +130,189 @@ full = False
 FPS = int(f[3])
 L = round(300/FPS)
 
+font = pygame.font.Font('freesansbold.ttf', 32)
+title = font.render('Rise Of Ritler', False, (255,255,255))
+title_size = (title.get_width(), title.get_height())
+
+settings = None 
+
+
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            x, y = wn.get_size()
-            with open('Assets/MEM/Settings.txt', 'r') as f:
-                g = f.read().split('\n')
-                f.close()
-            with open('Assets/MEM/Settings.txt', 'w') as f:
-                if full:
-                    g[1] = 'True'
-                else:
-                    g[0] = f'{x}x{y}'
-                f.write('\n'.join(g))
-                f.close()
-            pygame.quit()
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                text += 'w'
-            if event.key == pygame.K_a:
-                text += 'a'
-            if event.key == pygame.K_s:
-                text += 's'
-            if event.key == pygame.K_d:
-                text += 'd'
-            if event.key == pygame.K_F11:
-                if full:
-                    with open('Assets/MEM/Settings.txt', 'r') as f:
-                        g = f.read().split('\n')
-                        f.close()
-                        size = []
-                        g[0] = g[0].split('x')
-                        for i in range(len(g[0])):
-                            size.append(int(g[0][i]))
-                    with open('Assets/MEM/Settings.txt', 'w') as f:
-                        g[1] = 'False'
-                        x, y = wn.get_size()
-                        g[0] = f'{x}x{y}'
-                        f.write('\n'.join(g))
-                        f.close()
-                    wn = pygame.display.set_mode(tuple(size), pygame.RESIZABLE)
-                    full = False
-                else:
-                    with open('Assets/MEM/Settings.txt', 'r') as f:
-                        g = f.read().split('\n')
-                        f.close()
-                    with open('Assets/MEM/Settings.txt', 'w') as f:
+    if state == 'game':
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                x, y = wn.get_size()
+                with open('Assets/MEM/Settings.txt', 'r') as f:
+                    g = f.read().split('\n')
+                    f.close()
+                with open('Assets/MEM/Settings.txt', 'w') as f:
+                    if full:
                         g[1] = 'True'
-                        x, y = wn.get_size()
-                        g[0] = f'{x}x{y}'
-                        f.write('\n'.join(g))
-                        f.close()
-                    wn = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, pygame.RESIZABLE)
-                    full = True
-            if event.key == pygame.K_LSHIFT:
-                ke = True
-            
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                text.remove('w')
-            if event.key == pygame.K_a:
-                text.remove('a')
-            if event.key == pygame.K_s:
-                text.remove('s')
-            if event.key == pygame.K_d:
-                text.remove('d')
-            if event.key == pygame.K_LSHIFT:
-                ke = False
-
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            thing = False
-            bot_clicked = []
-            num = []
-            for i in range(2):
-                num.append(pos[i] - jews[i])
-            for bot in bots:
-                if check_collision(bot.pos(), num):
-                    if bot.team == 'e' or bot.team == 'me' and bool(len(cols)):
-                        for col in cols:
-                            col.target = bot
-                            col.target_type = 'r'
-                            print('Target')
-                        cols = []
-                            
                     else:
-                        cols.append(bot)
-                        bot_clicked.append(bot)
-                        thing = True
-                        print('Sel')
+                        g[0] = f'{x}x{y}'
+                    f.write('\n'.join(g))
+                    f.close()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    text += 'w'
+                if event.key == pygame.K_a:
+                    text += 'a'
+                if event.key == pygame.K_s:
+                    text += 's'
+                if event.key == pygame.K_d:
+                    text += 'd'
+                if event.key == pygame.K_F11:
+                    if full:
+                        with open('Assets/MEM/Settings.txt', 'r') as f:
+                            g = f.read().split('\n')
+                            f.close()
+                            size = []
+                            g[0] = g[0].split('x')
+                            for i in range(len(g[0])):
+                                size.append(int(g[0][i]))
+                        with open('Assets/MEM/Settings.txt', 'w') as f:
+                            g[1] = 'False'
+                            x, y = wn.get_size()
+                            g[0] = f'{x}x{y}'
+                            f.write('\n'.join(g))
+                            f.close()
+                        wn = pygame.display.set_mode(tuple(size), pygame.RESIZABLE)
+                        full = False
+                    else:
+                        with open('Assets/MEM/Settings.txt', 'r') as f:
+                            g = f.read().split('\n')
+                            f.close()
+                        with open('Assets/MEM/Settings.txt', 'w') as f:
+                            g[1] = 'True'
+                            x, y = wn.get_size()
+                            g[0] = f'{x}x{y}'
+                            f.write('\n'.join(g))
+                            f.close()
+                        wn = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, pygame.RESIZABLE)
+                        full = True
+                if event.key == pygame.K_LSHIFT:
+                    ke = True
+                
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    text.remove('w')
+                if event.key == pygame.K_a:
+                    text.remove('a')
+                if event.key == pygame.K_s:
+                    text.remove('s')
+                if event.key == pygame.K_d:
+                    text.remove('d')
+                if event.key == pygame.K_LSHIFT:
+                    ke = False
+
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                thing = False
+                bot_clicked = []
+                num = []
+                for i in range(2):
+                    num.append(pos[i] - jews[i])
+                for bot in bots:
+                    if check_collision(bot.pos(), num):
+                        if bot.team == 'e' or bot.team == 'me' and bool(len(cols)):
+                            for col in cols:
+                                col.target = bot
+                                col.target_type = 'r'
+                                print('Target')
+                            cols = []
+                                
+                        else:
+                            cols.append(bot)
+                            bot_clicked.append(bot)
+                            thing = True
+                            print('Sel')
+                        
+                if cols != [] and not ke:
+                    for col in cols:
+                        if col not in bot_clicked:
+                            if col.team == 's' or 'me':
+                                col.target = [pos[0]-jews[0], pos[1]-jews[1]]
+                                col.update_target()
+                            elif col.team == 'e':
+                                pass
                     
-            if cols != [] and not ke:
-                for col in cols:
-                    if col not in bot_clicked:
-                        if col.team == 's' or 'me':
-                            col.target = [pos[0]-jews[0], pos[1]-jews[1]]
-                            col.update_target()
-                        elif col.team == 'e':
-                            pass
-                
-            if not ke and not thing:
-                cols = []
-                
-            bot_clicked = []
+                if not ke and not thing:
+                    cols = []
+                    
+                bot_clicked = []
 
-    for hitler in text:
-        if hitler == 'w':
-            jews[1] += L
-        if hitler == 'a':
-            jews[0] += L
-        if hitler == 's':
-            jews[1] -= L
-        if hitler == 'd':
-            jews[0] -= L
-            
-    for bot in bots:
-        bot.update()
-        wn.blit(bot.surface, (bot.x+jews[0]-10, bot.y+jews[1]-10))
-    pygame.display.update()
-    clock.tick(FPS)
-    #print(clock.get_fps())
+        for hitler in text:
+            if hitler == 'w':
+                jews[1] += L
+            if hitler == 'a':
+                jews[0] += L
+            if hitler == 's':
+                jews[1] -= L
+            if hitler == 'd':
+                jews[0] -= L
+                
+        for bot in bots:
+            bot.update()
+            wn.blit(bot.surface, (bot.x+jews[0]-10, bot.y+jews[1]-10))
+        pygame.display.update()
+        clock.tick(FPS)
+        #print(clock.get_fps())
 
-    wn.fill('black')
+        wn.fill('black')
+    if state == 'menu':
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                x, y = wn.get_size()
+                with open('Assets/MEM/Settings.txt', 'r') as f:
+                    g = f.read().split('\n')
+                    f.close()
+                with open('Assets/MEM/Settings.txt', 'w') as f:
+                    if full:
+                        g[1] = 'True'
+                    else:
+                        g[0] = f'{x}x{y}'
+                    f.write('\n'.join(g))
+                    f.close()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:
+                    if full:
+                        with open('Assets/MEM/Settings.txt', 'r') as f:
+                            g = f.read().split('\n')
+                            f.close()
+                            size = []
+                            g[0] = g[0].split('x')
+                            for i in range(len(g[0])):
+                                size.append(int(g[0][i]))
+                        with open('Assets/MEM/Settings.txt', 'w') as f:
+                            g[1] = 'False'
+                            x, y = wn.get_size()
+                            g[0] = f'{x}x{y}'
+                            f.write('\n'.join(g))
+                            f.close()
+                        wn = pygame.display.set_mode(tuple(size), pygame.RESIZABLE)
+                        full = False
+                    else:
+                        with open('Assets/MEM/Settings.txt', 'r') as f:
+                            g = f.read().split('\n')
+                            f.close()
+                        with open('Assets/MEM/Settings.txt', 'w') as f:
+                            g[1] = 'True'
+                            x, y = wn.get_size()
+                            g[0] = f'{x}x{y}'
+                            f.write('\n'.join(g))
+                            f.close()
+                        wn = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, pygame.RESIZABLE)
+                        full = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
+        size = wn.get_size()
+        coords = (size[0]//2 - title_size[0]//2, size[1]-size[1]*0.9)
+        wn.blit(title, coords)
+        pygame.display.update()
+        clock.tick(FPS)
+        wn.fill('black')
